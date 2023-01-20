650e3927bd2ac451833d56ab7388f153526be2bd
Fabian Ruhland <ruhland@hhu.de>
2023-01-20 13:18:33 +0100

    SocketChannel: Use 3 bytes each for 'message length' and 'read bytes' to save 2 bytes header space

--- core/src/main/java/de/hhu/bsinfo/hadronio/util/MessageUtil.java
+++ core/src/main/java/de/hhu/bsinfo/hadronio/util/MessageUtil.java
@@ -1,25 +1,24 @@
 package de.hhu.bsinfo.hadronio.util;
 
+import de.hhu.bsinfo.hadronio.generated.DebugConfig;
 import org.agrona.MutableDirectBuffer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
 
 public class MessageUtil {
 
-    public static final int HEADER_LENGTH = 2 * Integer.BYTES + Short.BYTES;
+    public static final int HEADER_LENGTH = 3 + 3 + (DebugConfig.DEBUG ? Short.BYTES : 0);
     public static final int MESSAGE_OFFSET_LENGTH = 0;
-    public static final int MESSAGE_OFFSET_READ_BYTES = Integer.BYTES;
-    public static final int MESSAGE_OFFSET_SEQUENCE_NUMBER = 2 * Integer.BYTES;
+    public static final int MESSAGE_OFFSET_READ_BYTES = 3;
+    public static final int MESSAGE_OFFSET_SEQUENCE_NUMBER = 2 * 3;
     public static final int MESSAGE_OFFSET_DATA = HEADER_LENGTH;
 
     public static int getMessageLength(final MutableDirectBuffer buffer, final int index) {
-        return buffer.getInt(index + MESSAGE_OFFSET_LENGTH);
+        return read24BitInt(buffer, index + MESSAGE_OFFSET_LENGTH);
     }
 
     public static int getReadBytes(final MutableDirectBuffer buffer, final int index) {
-        return buffer.getInt(index + MESSAGE_OFFSET_READ_BYTES);
+        return read24BitInt(buffer, index + MESSAGE_OFFSET_READ_BYTES);
     }
 
     public static short getSequenceNumber(final MutableDirectBuffer buffer, final int index) {
@@ -46,11 +45,11 @@ public class MessageUtil {
     }
 
     public static void setMessageLength(final MutableDirectBuffer buffer, final int index, final int value) {
-        buffer.putInt(index + MESSAGE_OFFSET_LENGTH, value);
+        write24BitInt(buffer, index + MESSAGE_OFFSET_LENGTH, value);
     }
 
     public static void setReadBytes(final MutableDirectBuffer buffer, final int index, final int value) {
-        buffer.putInt(index + MESSAGE_OFFSET_READ_BYTES, value);
+        write24BitInt(buffer, index + MESSAGE_OFFSET_READ_BYTES, value);
     }
 
     public static void setSequenceNumber(final MutableDirectBuffer buffer, final int index, final short value) {
@@ -59,5 +58,14 @@ public class MessageUtil {
 
     public static void setMessageData(final MutableDirectBuffer targetBuffer, final int targetIndex, final ByteBuffer sourceBuffer, final int messageLength) {
         targetBuffer.putBytes(targetIndex + MESSAGE_OFFSET_DATA, sourceBuffer, sourceBuffer.position(), messageLength);
+    }
+
+    private static int read24BitInt(final MutableDirectBuffer buffer, final int index) {
+        return (buffer.getShort(index) & 0xffff) | ((buffer.getByte(index + 2) & 0xff) << 16);
+    }
+
+    private static void write24BitInt(final MutableDirectBuffer buffer, final int index, final int value) {
+        buffer.putShort(index, (short) value);
+        buffer.putByte(index + 2, (byte) (value >> 16));
     }
 }
